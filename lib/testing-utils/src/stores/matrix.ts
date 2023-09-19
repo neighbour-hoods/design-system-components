68bcdbcc57343063fe76b0e076054bcc179d5e36
Nick Stebb <nickolarse@riseup.net>
2023-09-19 15:42:46 +1200

    start building out matrix store api/options

--- lib/testing-utils/src/stores/matrix.ts
+++ lib/testing-utils/src/stores/matrix.ts
@@ -2,14 +2,23 @@ import { LitElement, html } from 'lit';
 import { property } from 'lit/decorators.js';
 import { contextProvider } from '@lit-labs/context';
 import { matrix } from './contexts';
+import { vi } from 'vitest';
+import { ConfigDimension } from '@neighbourhoods/client';
+import { DimensionPairNames, generateMockDimensionsResponse } from '../factories/dimension';
+import { encode } from '@msgpack/msgpack';
 
-export class TestHarness extends LitElement {
+type NestedStore = 'profiles' | 'sensemaker';
+
+export interface MockMatrixStoreOptions {
+  includeStores: NestedStore[]
+  includeDimensions: DimensionPairNames[]
+};
+export class MatrixTestHarness extends LitElement {
   /**
-   * Providing a context at the root element to maintain application state
+   * Providing a context at the root element to maintain state in the tested component
    */
   @contextProvider({ context: matrix })
   @property({ attribute: false })
-  // Create a mock store with the mock data
   _matrixStore! :any;
 
   render() {
@@ -18,6 +27,38 @@ export class TestHarness extends LitElement {
 }
 
 const MockMatrixStore = {
+  mockDimensions: [''],
+  init(options: MockMatrixStoreOptions){
+    const store : any = {};
+
+    if(options.includeDimensions.length > 0) {
+      this.mockDimensions = generateMockDimensionsResponse(options.includeDimensions).map((dimension: ConfigDimension) => JSON.stringify(dimension))
+    }
+    if(options.includeStores.includes('profiles')) {
+    }
+    if(options.includeStores.includes('sensemaker')) {
+    }
+    
+    // Set up mocks for direct zome calls as needed/implemented by your component
+    // const mockClient = () => ({
+    //   appInfo: vi.fn(() => ({
+    //     cell_info: {
+    //       sensemaker: [null, { cloned: { cell_id: 'mock-cell-id' } }],
+    //     },
+    //   })),
+    //   callZome: vi.fn(({functionName} : any) => {
+    //     switch (functionName) {
+    //       case 'get_dimensions':
+    //         return this.mockDimensions.map((dimension : string) => ({entry: { Present: { 'entry': encode({...dimension}) } }}))
+    //       default:
+    //         break;
+    //     }
+    //   }),
+    // });
+    // store.client = mockClient();
+
+return 'hi'
+  },
   wrap(component: any) {
 
   },

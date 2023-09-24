335cf598adbe2e031a658d206930f6c0b5ef5909
Nick Stebb <nickolarse@riseup.net>
2023-09-25 11:56:19 +1300

    test for mocked profile

--- lib/testing-utils/src/stores/__tests__/nested-profile.test.ts
+++ lib/testing-utils/src/stores/__tests__/nested-profile.test.ts
@@ -3,9 +3,9 @@ import stores from '..';
 const { JSDOM } = require('jsdom');
 import { describe, expect, test, beforeEach, afterEach } from 'vitest';
 import { MatrixTestHarness, MockMatrixStoreOptions } from '../matrix';
-import { ConfigDimension } from '@neighbourhoods/client';
 import './test-component'
 import { TestComponent } from './test-component'
+import hash from '@neighbourhoods/testing-utils/factories/hash';
 
 /**
  * @vitest-environment jsdom
@@ -56,6 +56,33 @@ describe('When a mock Matrix store is initialised with options for a nested Prof
 
     test(`And the Matrix store has a mock profilesStore method`, async () => {
       expect(typeof component._matrixStore?.profilesStore).toBe('function')
+    });
+
+    test(`And calling the profilesStore method with different WeGroupIds gives you different stores (TODO)`, async () => {
+      // expect(typeof component._matrixStore?.profilesStore).toBe('function')
+    });
+
+    test(`And calling the profilesStore method gives you a store`, async () => {
+      expect(typeof component._matrixStore?.profilesStore()).toBe('object')
+    });
+
+    test(`And the store has a profiles Map`, async () => {
+      expect(component._matrixStore?.profilesStore().profiles).toBeDefined
+      expect(component._matrixStore?.profilesStore().profiles instanceof Map).toBeTruthy
+    });
+
+    test(`And Given a hash, When you call *get* with the hash, Then the profiles Map returns a value`, async () => {
+      const getResponse = component._matrixStore?.profilesStore().profiles.get(hash());
+      expect(getResponse).toBeDefined
+    });
+    
+    test(`And the value is a mock async readable of a basic Profile`, async () => {
+      const getResponse = component._matrixStore?.profilesStore().profiles.get(hash());
+      expect(getResponse instanceof Object).toBe(true)
+      expect(getResponse.status).toBe('complete')
+      
+      expect(getResponse.value.nickname).toBeDefined()
+      expect(typeof getResponse.value.nickname).toBe('string')
     });
     
   });

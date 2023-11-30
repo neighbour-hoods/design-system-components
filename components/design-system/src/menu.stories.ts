e0bb29bae8fb0ca2f89d316adb99bd7365f02b48
Nicholas Stebbings <nickolarse@riseup.net>
2023-11-30 16:25:12 +1300

    fix error for page-header from removing  imported stories

--- components/design-system/src/menu.stories.ts
+++ components/design-system/src/menu.stories.ts
@@ -1,6 +1,8 @@
+import { literal } from 'lit/static-html.js';
 import NHMenu from "./menu";
 import { html } from "lit";
 import type { Meta, StoryObj } from "@storybook/web-components";
+import { b64images } from '@neighbourhoods/design-system-styles';
 
 customElements.define('nh-menu', NHMenu)
 
@@ -23,5 +25,70 @@ type Story = StoryObj<MenuProps>;
 
 export const Default: Story = {
   args: {
+  },
+};
+export const HorizontalButtons: Story = {
+  args: {
+    direction: "horizontal",
+    itemLabels: ["Posts", "Popular", "Recent", "Review"],
+    itemComponentTag: literal`nh-button`,
+    itemComponentProps: { variant: "primary", size: "lg" },
+    fixedFirstItem: false,
+    addItemButton: false,
+    theme: "dark",
+  },
+};
+
+export const VerticalButtons: Story = {
+  args: {
+    direction: "vertical",
+    itemLabels: ["Posts", "Popular", "Recent", "Review"],
+    itemComponentTag: literal`nh-button`,
+    itemComponentProps: { variant: "primary", size: "stretch" },
+    fixedFirstItem: false,
+    addItemButton: false,
+    theme: "dark",
+  },
+};
+
+export const DashboardIconButtons: Story = {
+  args: {
+    direction: "horizontal",
+    itemLabels: ["", ""],
+    itemComponentTag: literal`nh-button`,
+    itemComponentProps: {
+      variant: "primary",
+      iconImageB64: b64images.icons.refresh,
+      size: "icon",
+    },
+    fixedFirstItem: false,
+    addItemButton: false,
+    theme: "dark",
+  },
+};
+export const HorizontalIconButtons: Story = {
+  args: {
+    direction: "horizontal",
+    itemLabels: ["Posts", "Popular", "Recent", "Review"],
+    itemComponentTag: literal`nh-button`,
+    itemComponentProps: {
+      variant: "primary",
+      iconImageB64: b64images.icons.refresh,
+      size: "lg",
+    },
+    fixedFirstItem: false,
+    addItemButton: false,
+    theme: "dark",
+  },
+};
+export const HorizontalTabButtons: Story = {
+  args: {
+    fixedFirstItem: true,
+    direction: "horizontal",
+    itemLabels: ["Posts", "Popular", "Recent", "Review"],
+    itemComponentTag: literal`nh-tab-button`,
+    itemComponentProps: { size: "lg" },
+    addItemButton: true,
+    theme: "dark",
   },
 };
